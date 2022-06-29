93bae6dcaa943082036b520e1d8528c236ff50f4
Landon Haugh <landon.haugh@nxp.com>
2022-06-29 16:39:37 -0500

    Add some script stuff

--- scripts/launch_example.sh
+++ scripts/launch_example.sh
@@ -5,8 +5,10 @@ echo "Waiting 1 second to restart bus"
 sleep 1
 sudo ip link set can0 up type can bitrate 1000000 dbitrate 4000000 loopback off fd on fd-non-iso off restart-ms 100
 echo "Waiting 5 seconds for UCAN to come online"
+candump can0 &
 sleep 5
 python3 ROS2CyphalMessageSubscriptionToSocketCANExample.py &
+sleep 3
 python3 ROS2CyphalMessagePCAExample.py &
-python3 ROS2CyphalMessageSPILEDPublisherExample.py &
-
+sleep 6
+pkill candump
